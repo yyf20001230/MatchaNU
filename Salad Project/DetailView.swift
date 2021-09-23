dcd0e513dccc127f2507397499cf252afe4c1c14
Frank Yang <yyf20001230@gmail.com>
2021-09-22 19:17:31 -0500

    button styling

--- Salad Project/DetailView.swift
+++ Salad Project/DetailView.swift
@@ -40,38 +40,33 @@ struct DetailView: View {
                         .clipShape(RoundedRectangle(cornerRadius: 5))
                         
                     }
-                    if !(self.classes.userClass.contains(self.classes.detail[0])){
-                        Button("ADD"){
-                            let classlocation = MKPointAnnotation()
-                            classlocation.coordinate = CLLocationCoordinate2D(latitude: classes.detail[0].ClassLocation[0], longitude: classes.detail[0].ClassLocation[1])
-                            classlocation.title = classes.detail[0].Major.components(separatedBy: " ")[0] + " " + classes.detail[0].Class.components(separatedBy: " ")[0] + "\n"
-                            classlocation.subtitle = classes.detail[0].MeetingInfo + "\n\n"
-                            self.classes.classlocations.append(classlocation)
-                            self.classes.userClass.append(self.classes.detail[0])
-                            self.showAlert = true
-                        }
-                        .alert(isPresented: self.$showAlert){
-                            Alert(title: Text("Class Dropped"), message: Text("\(classes.detail[0].Major.components(separatedBy: " ")[0] + " " + classes.detail[0].Class.components(separatedBy: " ")[0]) is dropped"), dismissButton: .default(Text("Got it!")))
-                        }
-                        .padding(.all)
-                        .accentColor(Color(#colorLiteral(red: 0.4745098039, green: 0.768627451, blue: 0.5843137255, alpha: 1)))
-                        .background(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                    }
-                    else{
-                        Button("DROP"){
-                            self.classes.userClass = self.classes.userClass.filter{$0 != self.classes.detail[0]}
-                            self.showAlert = true
-                        }
-                        .alert(isPresented: self.$showAlert){
-                            Alert(title: Text("Class Added"), message: Text("\(classes.detail[0].Major.components(separatedBy: " ")[0] + " " + classes.detail[0].Class.components(separatedBy: " ")[0]) is added to your schedule"), dismissButton: .default(Text("Got it!")))
-                        }
-                        .padding(.all)
-                        .accentColor(Color(#colorLiteral(red: 0.9176470588, green: 0.3450980392, blue: 0.3019607843, alpha: 1)))
-                        .background(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                    }
                     
+                    Button(action: {
+                        let classlocation = MKPointAnnotation()
+                        classlocation.coordinate = CLLocationCoordinate2D(latitude: classes.detail[0].ClassLocation[0], longitude: classes.detail[0].ClassLocation[1])
+                        classlocation.title = classes.detail[0].Major.components(separatedBy: " ")[0] + " " + classes.detail[0].Class.components(separatedBy: " ")[0] + "\n"
+                        classlocation.subtitle = classes.detail[0].MeetingInfo + "\n\n"
+                        self.classes.classlocations.append(classlocation)
+                        self.classes.userClass.append(self.classes.detail[0])
+                        self.showAlert = true
+                    }){
+                        VStack{
+                            Image(systemName: "plus.circle.fill")
+                                .rotationEffect(classes.userClass.contains(classes.detail[0]) ? Angle(degrees: 0) : Angle(degrees: 45))
+                                .padding(.top)
+                                .padding(.horizontal, 16)
+                                .scaleEffect(1.5)
+                                .scaledToFill()
+                                .shadow(color: Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.1), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                            Text(classes.userClass.contains(classes.detail[0]) ? "ADD" : "DROP")
+                                .padding(.all)
+                                .font(.system(.caption2, design: .rounded))
+                        }
+                        .accentColor(self.classes.showRoute ? Color(#colorLiteral(red: 0.9176470588, green: 0.3450980392, blue: 0.3019607843, alpha: 1)) : Color(#colorLiteral(red: 0.4745098039, green: 0.768627451, blue: 0.5843137255, alpha: 1)))
+                        .background(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                    }
+
                 }
                 
                 if self.classes.detail[0].ClassLocation == [42.05780619999999,-87.67587739999999]{
