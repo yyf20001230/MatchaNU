f5e1c1914799979d9ece433faa099226d5313860
kevinsu2024 <su.kevin8@gmail.com>
2022-02-10 00:31:51 -0600

    stuff

--- Salad Project/DetailView.swift
+++ Salad Project/DetailView.swift
@@ -59,6 +59,7 @@ struct DetailView: View {
                         .font(.system(.subheadline, design: .rounded))
                         .tracking(-0.5)
                         .padding(.leading)
+                        
                     
                     Button(action: {
                         if classes.detail[0].ClassLocation[0] == -1 || classes.detail[0].ClassLocation[1] == -1{
